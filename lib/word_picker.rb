10405e4e1d6fa13bfacd77a84c0a58b3aa7358a6
Sinclair Target <sinclairtarget@gmail.com>
2015-12-11 00:07:04 -0500

    Working mvp.

---
+++ lib/word_picker.rb
@@ -0,0 +1,60 @@
+class WordPicker
+  def initialize(words, attempts: 4)
+    @possible_words = words
+    @choices = []
+    @attempts_left = 4
+    @last_pick = ""
+  end
+
+  def pick_word
+    return @last_pick, true, true if @possible_words.length == 0
+
+    @attempts_left -= 1
+
+    @last_pick = pick = @possible_words.delete(@possible_words.first)
+    is_last_pick = (@attempts_left == 0)
+    is_certain = (@possible_words.length == 0)
+    return pick, is_last_pick, is_certain
+  end
+
+  def set_likeness(word, likeness)
+    @choices << { word: word, likeness: likeness }
+    cull_possible_words
+  end
+
+  def to_s
+    @possible_words.inspect 
+  end
+
+  private
+  def likeness(w1, w2)
+    likeness = 0
+
+    w1.chars.each_with_index do |c, index|
+      likeness += 1 if c == w2[index]
+    end
+
+    likeness
+  end
+
+  def cull_possible_words
+    @possible_words.delete_if do |word|
+      @choices.any? do |choice|
+        likeness(word, choice[:word]) != choice[:likeness]
+      end
+    end
+  end
+
+  # Looking to exclude words until there is only one
+  # word - likeness hash
+  # constraint satisfaction problem
+  # when you get back a likeness value, what does it tell you?
+  # the number of correctly filled blanks
+  # Wp contains L letters from Wg at their positions
+  # (|Wg| choose L) possiblities
+  #
+  # Wp = Warm
+  # Wg = Wood, Gosh, Ball, Wrap, Warn
+  #       1     0     1     1     3
+  # Wg = Gnat
+end
--- master_hack.rb
+++ master_hack.rb
@@ -1,7 +1,10 @@
+require_relative 'lib/word_picker'
+
 words = []
+system("clear")
 
 begin
-  puts "Enter the next available word: [EOF to finish]"
+  puts "Enter a potential password: [EOF to finish]"
   next_word = gets.to_s.chomp
   received_valid_input = next_word != ''
   words << next_word.chomp if received_valid_input
@@ -9,4 +12,18 @@ end while received_valid_input
 
 exit(0) if words.length == 0
 
-puts words.inspect
+picker = WordPicker.new(words)
+
+begin
+  system("clear")
+  puts "Potential passwords: #{picker}"
+  pick, done, is_certain = picker.pick_word
+  if is_certain
+    puts "The password is '#{pick}'."
+  else
+    puts "Try '#{pick}'."
+    puts "What was the likeness?"
+    likeness = gets.chomp.to_i
+    picker.set_likeness pick, likeness
+  end
+end while not done
