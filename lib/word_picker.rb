51ab9260f985f5bd787dddab8ffab739b2d571ef
Sinclair Target <sinclairtarget@gmail.com>
2015-12-12 19:51:12 -0500

    Next pick implementation.

--- lib/word_picker.rb
+++ lib/word_picker.rb
@@ -8,7 +8,7 @@ class WordPicker
   def pick_word
     return @last_pick, true if @possible_words.length == 0
 
-    @last_pick = pick = @possible_words.delete(@possible_words.first)
+    @last_pick = pick = next_pick()
     is_certain = (@possible_words.length == 0)
     return pick, is_certain
   end
@@ -33,6 +33,39 @@ class WordPicker
   end
 
   private
+  def next_pick
+    max_score = -1
+    pick = nil
+    @possible_words.each do |word|
+      word_score = score(word)
+      
+      if word_score > max_score
+        pick = word
+        max_score = word_score
+      end
+    end
+
+    @possible_words.delete pick
+  end
+
+  def score(word)
+    other_words_count = @possible_words.length - 1
+    return 0 if other_words_count == 0
+
+    likeness_hash = Hash.new(0)
+    @possible_words.each do |other_word|
+      next if other_word == word
+
+      likeness = WordPicker.likeness(word, other_word)
+      likeness_hash[likeness] += 1
+    end
+
+    likenesses_count = likeness_hash.values.length
+    (likeness_hash.values.inject(0) do |sum, el|
+      sum + (other_words_count - el)
+    end).to_f / likenesses_count
+  end
+
   def cull_possible_words
     @possible_words.delete_if do |word|
       @choices.any? do |choice|
